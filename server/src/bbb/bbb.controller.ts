631a27d591059c502e004e495ffa54edb90b5f44
李云富 <liyunfu1998@foxmail.com>
2023-10-04 16:32:32 +0800

    feat: 全局模块

---
+++ server/src/bbb/bbb.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { BbbService } from './bbb.service';
+import { CreateBbbDto } from './dto/create-bbb.dto';
+import { UpdateBbbDto } from './dto/update-bbb.dto';
+import { AaaService } from 'src/aaa/aaa.service';
+
+@Controller('bbb')
+export class BbbController {
+  constructor(
+    private readonly bbbService: BbbService,
+    private readonly aaaService: AaaService,
+  ) {}
+
+  @Post()
+  create(@Body() createBbbDto: CreateBbbDto) {
+    return this.bbbService.create(createBbbDto);
+  }
+
+  @Get()
+  findAll() {
+    return `this action returns all bbb` + this.aaaService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bbbService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateBbbDto: UpdateBbbDto) {
+    return this.bbbService.update(+id, updateBbbDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.bbbService.remove(+id);
+  }
+}
