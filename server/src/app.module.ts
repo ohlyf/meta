8eca2bad3056c9be45115df38d4db25904e24b95
李云富 <liyunfu1998@foxmail.com>
2023-10-04 16:19:18 +0800

    feat: 使用provide示例

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,6 +6,40 @@ import { PersonModule } from './person/person.module';
 @Module({
   imports: [PersonModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: AppService,
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: '章三',
+        age: 20,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'aaa',
+          desc: 'ccc',
+        };
+      },
+    },
+    {
+      provide: 'person3',
+      async useFactory(person: { name: string }, appService: AppService) {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+        return {
+          name: person.name,
+          desc: appService.getHello(),
+        };
+      },
+      inject: ['person', AppService],
+    },
+  ],
 })
 export class AppModule {}
