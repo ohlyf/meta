3d0385632808785b2aeae7a6fc31944e4a0525e3
liyunfu <liyunfu1998@foxmail.com>
2023-10-14 18:06:42 +0800

    feat: 学习RN基础组件

--- client/App.tsx
+++ client/App.tsx
@@ -1,3 +1,5 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 import { SafeAreaView, TextInput, Text } from "react-native";
 import React, { useState } from "react";
@@ -9,8 +11,18 @@ export default function App() {
   const ref3 = React.useRef<TextInput>(null);
 
   return (
-    <SafeAreaView>
-      <VerificationCode />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
