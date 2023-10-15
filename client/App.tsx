44b049609b2a0f961ef14b61cc9de79a5c851439
ohlyf <1511491804@qq.com>
2023-10-15 09:28:52 -0500

    feat: 修复app.tsx (#9)
    Co-authored-by: 李云富 <liyunfu0216@icloud.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,28 +1,9 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, Text } from "react-native";
 
-import { SafeAreaView, TextInput, Text } from "react-native";
-import React, { useState } from "react";
-import AutoNextFocusTextInputs from "./src/components/AutoNextFocusTextInputs";
-import VerificationCode from "./src/components/VerificationCode";
 export default function App() {
-  const ref1 = React.useRef<TextInput>(null);
-  const ref2 = React.useRef<TextInput>(null);
-  const ref3 = React.useRef<TextInput>(null);
-
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text>React App</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
