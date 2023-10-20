8c88252faf6df50168510f7163f67bbec9d54407
ohlyf <1511491804@qq.com>
2023-10-20 03:28:09 +0000

    feat: 添加图片选择

--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -3,13 +3,14 @@ import FontAwesome from '@expo/vector-icons/FontAwesome'
 
 interface Props {
   label: string,
-  theme?: 'primary' | 'secondary'
+  theme?: 'primary' | 'secondary',
+  onPress?: () => void
 }
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === 'primary') {
     return (
       <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }]}>
-        <Pressable style={[styles.button, { backgroundColor: '#fff' }]}>
+        <Pressable style={[styles.button, { backgroundColor: '#fff' }]} onPress={onPress}>
           <FontAwesome name='picture-o' size={18} color='#25292e' style={styles.buttonIcon} />
           <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
         </Pressable>
--- client/components/ImageViewer.tsx
+++ client/components/ImageViewer.tsx
@@ -1,7 +1,12 @@
 import { StyleSheet, Image } from "react-native";
 
-export default function ImageViewer({ placeholderImageSource }) {
-  return <Image source={placeholderImageSource} style={styles.image} />;
+interface Props {
+  placeholderImageSource: any,
+  selectedImage: string
+}
+export default function ImageViewer({ placeholderImageSource, selectedImage }: Props) {
+  const imageSource = selectedImage ? { uri: selectedImage } : placeholderImageSource
+  return <Image source={imageSource} style={styles.image} />;
 }
 
 const styles = StyleSheet.create({
