eef8ab23fd873acc8c1afd8a1a27cbf9dbe9f772
ohlyf <1511491804@qq.com>
2023-10-20 02:32:02 +0000

    feat: expo基础示例

--- client/App.tsx
+++ client/App.tsx
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from './components/Button'
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label='Choose a photo' theme="primary" />
+        <Button label='Use this photo' />
+      </View>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+    alignItems: 'center',
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  }
+
+})
